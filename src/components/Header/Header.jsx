3956c050ec6c409f2a22bc22b48a6c2876997431
DianaKryzhanivska <di_parkhomenko@ukr.net>
2024-02-20 17:23:38 +0200

    header template & styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { Span, StyledContainer } from './Header.styled';
+import {
+  AuthBox,
+  NameNavLink,
+  NavBar,
+  NavBarLink,
+  SignInBtn,
+  SignUpBtn,
+  Span,
+  StyledContainer,
+} from './Header.styled';
 
 const Header = () => {
   return (
     <>
       <header>
         <StyledContainer>
-          <NavLink to="/">
+          <NameNavLink to="/">
             <Span>psychologists.</Span>services
-          </NavLink>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/psychologists">Psychologists</NavLink>
-          <button>Log in</button>
-          <button>Registration</button>
+          </NameNavLink>
+          <NavBar>
+            <NavBarLink to="/">Home</NavBarLink>
+            <NavBarLink to="/psychologists">Psychologists</NavBarLink>
+          </NavBar>
+          <AuthBox>
+            <SignInBtn>Log in</SignInBtn>
+            <SignUpBtn>Registration</SignUpBtn>
+          </AuthBox>
         </StyledContainer>
       </header>
     </>
--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -1,17 +1,59 @@
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import { Container } from 'styles/GlobalStyles';
 
 export const StyledContainer = styled(Container)`
   padding-top: 24px;
   padding-bottom: 24px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const NameNavLink = styled(NavLink)`
+  font-size: 20px;
+  font-weight: 700;
+  line-height: 1.2em;
+  letter-spacing: -0.4px;
 `;
 
 export const Span = styled.span`
   color: ${({ theme }) => theme.colors.mainGreen};
 `;
 
-export const LoginBtn = styled.button`
+export const NavBar = styled.div`
+  display: flex;
+  gap: 40px;
+`;
+
+export const NavBarLink = styled(NavLink)`
+  font-weight: 400;
+  line-height: 1.25em;
+  letter-spacing: -0.16px;
+`;
+
+export const AuthBox = styled.div`
+  display: flex;
+  gap: 8px;
+`;
+
+export const SignInBtn = styled.button`
   padding: 14px 39px;
   border-radius: 30px;
   border: 1px solid rgba(25, 26, 21, 0.2);
+  color: ${({ theme }) => theme.colors.black};
+  background: transparent;
+  font-weight: 500;
+  line-height: 1.25em;
+  letter-spacing: -0.16px;
+`;
+
+export const SignUpBtn = styled.button`
+  padding: 14px 40px;
+  border-radius: 30px;
+  background-color: ${({ theme }) => theme.colors.mainGreen};
+  color: ${({ theme }) => theme.colors.white};
+  font-weight: 500;
+  line-height: 1.25em;
+  letter-spacing: -0.16px;
 `;
